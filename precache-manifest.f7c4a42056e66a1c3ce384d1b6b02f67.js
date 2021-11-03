b724a293ffec303b4c0f0d3230baa08dd6708db3
Emin Iandyrhanov <mia@bringme.com>
2021-11-03 03:02:30 +0100

    v0.1

---
+++ precache-manifest.f7c4a42056e66a1c3ce384d1b6b02f67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bfde9e51a774641ab069e6c110c516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b973c01694b6bb29014d",
+    "url": "/static/css/main.9136e423.chunk.css"
+  },
+  {
+    "revision": "63e432e912fcfc3c206e",
+    "url": "/static/js/2.f0dd8876.chunk.js"
+  },
+  {
+    "revision": "9141e6603b2ba65e9f944cb50ce12050",
+    "url": "/static/js/2.f0dd8876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bec5663a8b8a8de5897",
+    "url": "/static/js/3.8daf8e4b.chunk.js"
+  },
+  {
+    "revision": "b973c01694b6bb29014d",
+    "url": "/static/js/main.9c98bfb2.chunk.js"
+  },
+  {
+    "revision": "eb741a1e51f749f315d3",
+    "url": "/static/js/runtime-main.4949c907.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
